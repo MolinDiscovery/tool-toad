a79b2453f37525369234ea6b03cc5a6ff4397095
MolinDiscovery <jacob.molin@me.com>
2026-05-04 15:12:08 +0200

    Add g-xTB v2 calculator

--- tooltoad/__init__.py
+++ tooltoad/__init__.py
@@ -5,6 +5,7 @@ from .chemutils import *
 from .vis import *
 from .orca import orca_calculate
 from .xtb import xtb_calculate
+from .gxtb import gxtb_calculate
 from .periodictable import PeriodicTable
 from .crest import run_crest
 from .thermochemistry import *
@@ -12,6 +13,7 @@ from .thermochemistry import *
 # Backward-compatible short aliases used by some downstream code.
 orca = orca_calculate
 xtb = xtb_calculate
+gxtb = gxtb_calculate
 
 __all__ = [
     *getattr(chemutils, "__all__", []),
@@ -21,6 +23,8 @@ __all__ = [
     "orca_calculate",
     "xtb",
     "xtb_calculate",
+    "gxtb",
+    "gxtb_calculate",
     "PeriodicTable",
     "run_crest",
 ]
